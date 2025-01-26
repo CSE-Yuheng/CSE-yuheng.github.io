7322a7f2f0807faf9da95dfe2cd8acd3dad4730e
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-01-26 18:32:17 -0500

    Delete csce242/projects/part1/script.js

--- csce242/projects/part1/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('paletteForm').onsubmit = function(event) {
-    var name = document.getElementById('paletteName').value;
-    if (name.length < 3) {
-        alert('Palette name must be at least 3 characters long.');
-        event.preventDefault(); // Prevent form from submitting
-    }
-};
